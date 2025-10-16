c4d4984c4d0fb11c55b446cb78ed04087d6329bb
mchena <m.chena@itecriocuarto.org.ar>
2025-10-15 23:30:29 -0300

    ruta /games agregada

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ from models import (
     db
 )
 
+from views import (
+    GameAPI,
+    UserRegisterAPI,
+    AuthLoginAPI,
+    UserAPI,
+    UserDetailAPI,
+    ReviewAPI,
+    ReviewDetailAPI,
+    GenreAPI,
+    GenreDetailAPI,
+)
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     'mysql+pymysql://BD2021:BD2021itec@143.198.156.171:3306/argame_db'
@@ -14,4 +26,8 @@ app.config['JWT_SECRET_KEY'] = 'SECRET_KEY'
 jwt = JWTManager(app)
 db.init(app)
 
-
+app.add_url_rule(
+    '/games',
+    view_func=GameAPI.as_view('games_api'),
+    methods=['GET', 'POST']
+)
