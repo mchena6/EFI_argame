18763b63fc614c8fd0665836eebea4f8d20ad9f1
mchena <m.chena@itecriocuarto.org.ar>
2025-10-18 00:23:32 -0300

    Registro de usuarios y login corregidos

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, jsonify
 from marshmallow import ValidationError
 from flask.views import MethodView
 from flask_jwt_extended import (
@@ -30,17 +30,66 @@ from schemas import(
     GameSchema,
     GenreSchema,
     GameGenreSchema,
-    ReviewSchema
+    ReviewSchema,
+    RoleSchema,
+    RegisterSchema,
+    LoginSchema
 )
 
 class UserRegisterAPI(MethodView):
-    def post():
-        ...
+    def post(self):
+        try:
+            data = RegisterSchema().load(request.json)
+        except ValidationError as err:
+            return jsonify({"Error": err.messages}), 400
+        
+        if User.query.filter_by(email=data['email']).first():
+            return {"message": "Email en uso"}, 400
+        
+        new_user = User(
+            username=data['username'],
+            name=data['name'],
+            email=data['email']
+        )
+        db.session.add(new_user)
+        db.session.flush()
 
+        password_hash = bcrypt.hash(data['password'])
 
-class AuthLoginAPI():
-    def post():
-        ...
+        credentials = UserCredentials(
+            user_id=new_user.id,
+            password_hash=password_hash,
+            role_id=data['role_id']
+        )
+        db.session.add(credentials)
+        db.session.commit()
+        return UserSchema().dump(new_user), 201
+
+class AuthLoginAPI(MethodView):
+    def post(self):
+        try:
+            data = LoginSchema().load(request.json)
+        except ValidationError as err:
+            return{"Error": err.messages}, 400
+
+        user = User.query.filter_by(email=data['email']).first()
+        if not user or not user.credentials:
+            return {"message": "Credenciales invalidas"}, 401
+        if not bcrypt.verify(data['password'], user.credentials.password_hash):
+            return {"message": "Credenciales invalidas"}, 401
+        identity = str(user.id)
+        additional_claims = {
+            'id' : user.id,
+            'email' : user.email,
+            'username' : user.username,
+            'role' : user.credentials.role.name,
+        }
+        token = create_access_token(
+            identity=identity,
+            additional_claims=additional_claims
+        )
+        return {"access_token": token}, 200
+        
 
 class UserAPI(MethodView):
     # Traer usuarios (admin)
