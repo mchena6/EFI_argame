b2793bbde7aaf7add4458528e0e97a6707eb8256
mchena <m.chena@itecriocuarto.org.ar>
2025-11-17 20:52:25 -0300

    Metodos para GenreAPI agregados

--- repositories/genre_repository.py
+++ repositories/genre_repository.py
@@ -1,6 +1,19 @@
 from models import db, Genre
 
 class GenreRepository:
+    
     # Obtener genero por id
     def get_by_id(self, id):
         return Genre.query.get(id)
+    
+    # Buscar genero por nombre
+    def get_by_name(self, name):
+        return Genre.query.filter_by(name=name).first()
+
+    def get_all_active(self):
+    # Obtener una lista de todos los generos activos
+        return Genre.query.filter_by(is_active=True).all()
+    
+    # Agrega un nuevo género a la sesión.
+    def add(self, genre):
+        db.session.add(genre)
