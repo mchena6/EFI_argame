8950e5edf02ab8cd2e8b31e632e0c4a69dca6aa5
mchena <m.chena@itecriocuarto.org.ar>
2025-11-17 20:54:06 -0300

    Repositorio de review agregado

---
+++ repositories/review_repository.py
@@ -0,0 +1,19 @@
+from models import db, Review
+
+class ReviewRepository:
+    
+    # Obtener todas las reviews de un juego por su id
+    def get_by_game_id(self, game_id):    
+        return Review.query.filter_by(game_id=game_id).all()
+
+    # Obtiene una review por su id
+    def get_by_id(self, review_id):
+        return Review.query.get(review_id)
+
+    # Verificar si ya existe una review del usuario para ese juego.
+    def find_by_user_and_game(self, user_id, game_id):
+        return Review.query.filter_by(user_id=user_id, game_id=game_id).first()
+
+    # Agrega una nueva review a la base de datos
+    def add(self, review):
+        db.session.add(review)
